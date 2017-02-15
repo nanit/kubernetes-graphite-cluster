4a952cfddccb43c1a9bdf560402cac37039509be
Erez Rabih <erez.rabih@gmail.com>
2017-02-15 17:43:23 +0200

    initial commit

---
+++ statsd-proxy/proxyConfig.js
@@ -0,0 +1,14 @@
+{
+nodes: [
+{host: 'statsd-1', port: 8125, adminport: 8126},
+{host: 'statsd-2', port: 8125, adminport: 8126}
+],
+server: './servers/udp',
+host:  '0.0.0.0',
+port: 8125,
+mgmt_port: 8126,
+forkCount: 0,
+checkInterval: 1000,
+cacheSize: 10000
+}
+
