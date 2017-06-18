b4962edddbbf894f7ef7b70080f5f784e32f5a75
Erez Rabih <erez.rabih@gmail.com>
2017-06-18 13:48:10 +0300

    bash flags

--- docker/statsd-proxy/entrypoint.sh
+++ docker/statsd-proxy/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+set -e
 
 /set-cluster-nodes.sh
 exec node /app/statsd/proxy.js /app/statsd/proxyConfig.js
--- docker/statsd-proxy/set-cluster-nodes.sh
+++ docker/statsd-proxy/set-cluster-nodes.sh
@@ -2,6 +2,7 @@
 
 set -e
 set -u
+set -x
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
