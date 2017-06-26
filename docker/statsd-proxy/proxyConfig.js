5f7399ec4e0d286a8922eea5f6649ca477a11c58
Erez Rabih <erez.rabih@gmail.com>
2017-06-26 16:12:29 +0300

    graphite staging

--- docker/statsd-proxy/proxyConfig.js
+++ docker/statsd-proxy/proxyConfig.js
@@ -1,5 +1,9 @@
 {
-nodes: [@@STATSD_NODES@@],
+nodes: [
+  {host: '100.97.41.28', port: 8125, adminport:8126},
+  {host: '100.97.42.26', port: 8125, adminport:8126},
+  {host: '100.97.1.76', port: 8125, adminport:8126}
+],
 server: './servers/udp',
 host:  '0.0.0.0',
 port: 8125,
