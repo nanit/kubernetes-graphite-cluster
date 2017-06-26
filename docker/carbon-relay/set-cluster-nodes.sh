03a18ae2fb8222497250b9a5fa94053088fb7a95
Erez Rabih <erez.rabih@gmail.com>
2017-06-26 09:30:03 +0300

    fixed single instance cluster

--- docker/carbon-relay/set-cluster-nodes.sh
+++ docker/carbon-relay/set-cluster-nodes.sh
@@ -9,7 +9,7 @@ STATEFUL_SETS=$(curl -f -k https://kubernetes/apis/apps/v1beta1/statefulsets -H 
 GRAPHITE_NODES_SS=$(echo $STATEFUL_SETS | jq '.items[] | select(.metadata.name == "graphite-node")')
 REPLICAS=$(echo $GRAPHITE_NODES_SS | jq .spec.replicas)
 SERVICE_NAME=$(echo $GRAPHITE_NODES_SS | jq .spec.serviceName | tr -d '"')
-(( REPLICAS-= 1 ))
+REPLICAS=$((REPLICAS-1))
 NODES=()
 
 for i in $(seq 0 $REPLICAS)
